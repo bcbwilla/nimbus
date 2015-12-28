39ecb2b482483d735aea93dc25e64858e14e48c9
javipepe <javipepeoctc@gmail.com>
2015-12-28 23:43:46 +0100

    Update ball8.py

--- plugins/ball8.py
+++ plugins/ball8.py
@@ -4,14 +4,14 @@ from plugin import CommandPlugin, PluginException
 
 class Ball8(CommandPlugin):
     """
-    8ball command
+    8ball command (by javipepe :))
     """
 
     def __init__(self, bot):
         CommandPlugin.__init__(self, bot)
         self.triggers = ['8ball']
         self.short_help = 'Ask me a question'
-        self.help = 'Ask me a question, I\'ll decide what the answer should be. Based on https://en.wikipedia.org/wiki/Magic_8-Ball , adapted to Python by javipepe.'
+        self.help = 'Ask me a question, I\'ll decide what the answer should be. Based on https://en.wikipedia.org/wiki/Magic_8-Ball'
         self.help_example = ['!8ball Is linux better than windows?']
         #                                ^ obviously yes.
 
@@ -22,5 +22,5 @@ class Ball8(CommandPlugin):
         else:
             possible_answers = ['It is certain', 'It is decidedly so', 'Without a doubt', 'Yes, definitely', 'You may rely on it', 'As I see it, yes', 'Most likely', 'Outlook good', 'Yes', 'Signs point to yes', 'Reply hazy try again', 'Ask again later', 'Better not tell you now', 'Cannot predict now', 'Concentrate and ask again', 'Do\'t count on it', 'My reply is no', 'My sources say no', 'Outlook not so good', 'Very doubtful']
 
-            response['text'] = '8ball says *_%s_*!' % random.choice(possible_answers)
+            response['text'] = ':8ball: says *_%s_*!' % random.choice(possible_answers)
             self.bot.sc.api_call('chat.postMessage', **response)
